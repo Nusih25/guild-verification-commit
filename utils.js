28a2e877394866bd6be10a5d9b0c6457eb62318b
Zamazingoo <hulusi.tutuncu@gmail.com>
2026-03-05 21:53:04 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:52:54.956Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T18:53:03.903Z
+Commit: Fix race condition in async operations
