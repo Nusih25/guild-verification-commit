8b043d8ffa07a2fc142932a3e47a6ca7ff5434ff
Zamazingoo <hulusi.tutuncu@gmail.com>
2026-03-05 21:52:32 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:52:30.312Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T18:52:31.906Z
+Commit: Resolve timezone issues in date handling
