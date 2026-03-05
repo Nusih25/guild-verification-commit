5e571402ab50ce5d778e9d9577b34aa2fbd9a618
Zamazingoo <hulusi.tutuncu@gmail.com>
2026-03-05 21:49:11 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:48:56.194Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:49:11.361Z
+Commit: Correct typo in configuration file
