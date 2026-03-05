e03c1b3efa5b42e41b5268beb6d78134c306f64e
Zamazingoo <hulusi.tutuncu@gmail.com>
2026-03-05 21:52:05 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:51:59.007Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-05T18:52:05.157Z
+Commit: Fix CSS styling bug on mobile devices
