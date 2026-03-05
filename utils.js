9cf15386d8fa8b12384f51a9bc0da3701863b2f8
Zamazingoo <hulusi.tutuncu@gmail.com>
2026-03-05 21:52:55 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:52:27.357Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T18:52:54.956Z
+Commit: Fix memory leak in event handlers
