424fc244fa0f2466c755c3b740a4d45776e66173
Zamazingoo <hulusi.tutuncu@gmail.com>
2026-03-05 21:48:54 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:48:50.087Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T18:48:54.093Z
+Commit: Introduce real-time notifications
